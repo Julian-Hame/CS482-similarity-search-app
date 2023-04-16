9c302cc666e6628705a69ebd50cc9f8c3a68a30c
Julian <117950845+Julian-Hame@users.noreply.github.com>
2023-04-16 18:58:49 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+# Importing necessary modules
+import pickle
+import numpy as np
+from numpy.linalg import norm
+import os
+import time
+import pickle
+from tqdm import tqdm, tqdm_notebook
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+import streamlit as st
+from transformers import pipeline
+from PIL import Image
+
+# feature_dict = pickle.load(open('feature_dict.pickle', 'rb'))
+filenames = pickle.load(open('filenames.pickle', 'wb')
+print(filenames)
+# Turn image locations into actual person and image names
+'''
+person_names = []
+image_names = []
+x = 0
+while x < len(filenames):
+  split_name = filenames[x].split('/')
+  person_names.append(split_name[4].replace("_", " "))
+  image_names.append(split_name[5])
+  x = x + 1
+print("Person:   " +person_names[0])
+print("Filename: " +image_names[218])
+'''
+'''
+import streamlit as st
+from transformers import pipeline
+from PIL import Image
+st.title('Similar Image Finder')
+st.write("---")
+st.header('Select or upload an image')
+col1, col2 = st.columns(2)
+selection = "N/A"
+with col1:
+    option = st.selectbox(
+    'Pick an image',
+    ('pic1', 'pic2', 'pic3'))
+st.write('You selected:', option)
+with col2:
+    file_name = st.file_uploader("Upload your own image")
+st.write("---")
+'''
