55f32ccf5909a69d8882dbc9355033e87ee97722
Julian <117950845+Julian-Hame@users.noreply.github.com>
2023-04-16 22:34:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
-# Importing necessary modules
-import pickle
-import numpy as np
-from numpy.linalg import norm
-import os
-import time
-import pickle
-from tqdm import tqdm, tqdm_notebook
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 import streamlit as st
 from transformers import pipeline
+import pickle
 from PIL import Image
 
-# feature_dict = pickle.load(open('feature_dict.pickle', 'rb'))
-filenames = pickle.load(open('filenames.pickle', 'wb')
-print(filenames)
-# Turn image locations into actual person and image names
-'''
+st.title('Similar Faces Searcher')
+filenames = []
+filenames = pickle.load(open('filenames.pickle', 'rb'))
+
 person_names = []
 image_names = []
 x = 0
@@ -25,24 +15,25 @@ while x < len(filenames):
   person_names.append(split_name[4].replace("_", " "))
   image_names.append(split_name[5])
   x = x + 1
-print("Person:   " +person_names[0])
-print("Filename: " +image_names[218])
-'''
-'''
-import streamlit as st
-from transformers import pipeline
-from PIL import Image
-st.title('Similar Image Finder')
+    
 st.write("---")
 st.header('Select or upload an image')
 col1, col2 = st.columns(2)
 selection = "N/A"
+
 with col1:
-    option = st.selectbox(
-    'Pick an image',
-    ('pic1', 'pic2', 'pic3'))
-st.write('You selected:', option)
+    query_image = st.number_input('Enter the ID number of the dataset image you would like to use.', value = 1)
+    st.write('You selected:', image_names[query_image])
+
 with col2:
     file_name = st.file_uploader("Upload your own image")
 st.write("---")
-'''
+
+# Display query image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+%matplotlib inline
+
+st.write("Person:   " +person_names[query_image])
+st.write("Filename: " +image_names[query_image])
+plt.imshow(mpimg.imread(filenames[query_image]))
